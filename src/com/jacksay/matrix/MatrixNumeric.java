4ce214675ed51665be5104e85fb77735a25d490e
jacksay <jacksay@KITT>
2012-12-07 10:48:58 +0100

    - Add few comments

--- src/com/jacksay/matrix/MatrixNumeric.java
+++ src/com/jacksay/matrix/MatrixNumeric.java
@@ -1,10 +1,11 @@
 package com.jacksay.matrix;
 
 import java.util.Collection;
-import java.util.Iterator;
 
 /**
- *
+ * Specific matrix for numeric data. Provide functionalities for calculate 
+ * sum of lines, columns and global sum.
+ * 
  * @author jacksay
  */
 public class MatrixNumeric<E> extends Matrix<E, Double> {
@@ -17,21 +18,21 @@ public class MatrixNumeric<E> extends Matrix<E, Double> {
     public MatrixNumeric(Collection<E> rowsCols, Double defaultValue) {
         super(rowsCols, FREE, defaultValue);
     }
+    
+    public MatrixNumeric(Collection<E> rowsCols, boolean isDiagonal, Double defaultValue) {
+        super(rowsCols, isDiagonal, defaultValue);
+    }
 
     public MatrixNumeric(E[] rowsCols, Double[][] values, boolean isDiagonal, Double defaultValue) {
         super(rowsCols, values, isDiagonal, defaultValue);
     }
     
-    
-
-    @Override
-    public void setValue(E x, E y, Double value) {
-        super.setValue(x, y, value); //To change body of generated methods, choose Tools | Templates.
-    }
-    
-    
-    
-    
+    /**
+     * Return sum of all cells for line specified.
+     * 
+     * @param line
+     * @return double
+     */
     public double getTotalLine( E line )
     {
         double tmpTotalLine = 0.0;
@@ -41,6 +42,12 @@ public class MatrixNumeric<E> extends Matrix<E, Double> {
         return tmpTotalLine;
     }
     
+    /**
+     * Return sum of all cells for column specified.
+     * 
+     * @param column
+     * @return double
+     */
     public double getTotalColunm( E column )
     {
         double tmpTotalColumn = 0.0;
@@ -50,6 +57,11 @@ public class MatrixNumeric<E> extends Matrix<E, Double> {
         return tmpTotalColumn;
     } 
     
+    /**
+     * Return sum of all cells.
+     * 
+     * @return double
+     */
     public double getTotal()
     {
         double tmpTotalColumn = 0.0;
